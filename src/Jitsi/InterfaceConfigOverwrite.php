e52dc828a0a4539be57ac3f18f70cf8ad9a69891
Fabian Schmid <fs@studer-raimann.ch>
2020-04-21 07:54:35 +0200

    several configs (don't seem to work)

--- src/Jitsi/InterfaceConfigOverwrite.php
+++ src/Jitsi/InterfaceConfigOverwrite.php
@@ -9,7 +9,9 @@ namespace srag\Plugins\Jitsi\Jitsi;
 class InterfaceConfigOverwrite extends AbstractJasonable
 {
     public $SHOW_JITSI_WATERMARK = false;
+    public $DISABLE_VIDEO_BACKGROUND = true;
     public $JITSI_WATERMARK_LINK = '';
+    public $DEFAULT_BACKGROUND = '#000000';
     public $SHOW_WATERMARK_FOR_GUESTS = false;
     public $RECENT_LIST_ENABLED = false;
     public $SUPPORT_URL = '';
@@ -37,7 +39,7 @@ class InterfaceConfigOverwrite extends AbstractJasonable
     public $DISABLE_RINGING = true;
     public $MOBILE_APP_PROMO = false;
     public $DISABLE_TRANSCRIPTION_SUBTITLES = true;
-    public $VERTICAL_FILMSTRIP = false;
+    public $VERTICAL_FILMSTRIP = true;
     public $LOCAL_THUMBNAIL_RATIO = 16 / 9;
     public $REMOTE_THUMBNAIL_RATIO = 16 / 9;
 
